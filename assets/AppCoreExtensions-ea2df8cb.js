ab1277349ea2a0d1c32992b0822cb45795fcbd0c
tqtrung <tqtrung@humaxdigital.com>
2024-01-05 10:07:55 +0700

    commit

---
+++ assets/AppCoreExtensions-ea2df8cb.js
@@ -0,0 +1,2 @@
+import{C as a,W as e,S as s}from"./CoreExtension-3fd98810.js";function r(){import.meta.url,import("_").catch(()=>1);async function*o(){}}const t="./";class c extends a{async run(n){n.fontManager.addFontFace(new e("NotoSans",{},"/fonts/NotoSans-Regular.ttf")),n.fontManager.addFontFace(new s("Ubuntu",{},"msdf",n,t+"fonts/Ubuntu-Bold.msdf.png",t+"fonts/Ubuntu-Bold.msdf.json"))}}export{r as __vite_legacy_guard,c as default};
+//# sourceMappingURL=AppCoreExtensions-ea2df8cb.js.map
